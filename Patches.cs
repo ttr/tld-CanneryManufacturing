4e9f7a6970ae5879f9074b19b18ee18000cc4ab5
Varrxion <Jordan3827@gmail.com>
2024-12-11 21:01:03 -0500

    Update blueprint refresh to new method

--- Patches.cs
+++ Patches.cs
@@ -24,7 +24,7 @@ namespace CanneryManufacturingDLC
                     }
                 }
 
-                __instance.RefreshBlueprintDisplayList();
+                __instance.RefreshAvailableBlueprints();
             }
         }
 		
