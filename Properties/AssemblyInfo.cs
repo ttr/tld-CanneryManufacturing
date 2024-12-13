6d5e21e83366c1973e514566a81a629ad2dd31b2
Varrxion <Jordan3827@gmail.com>
2024-12-12 21:57:50 -0500

    Update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Runtime.InteropServices;
 //Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
 //Mods typically use 3 numbers. For example: 1.2.1
 //The mod version need specified in three places.
-[assembly: AssemblyVersion("1.7.1")]
-[assembly: AssemblyFileVersion("1.7.1")]
-[assembly: MelonInfo(typeof(CanneryManufacturingMain), "CanneryManufacturing", "1.7.1", "Digitalzombie, Atlas-Lumi, ds5678, Varrxion", null)]
+[assembly: AssemblyVersion("1.7.2")]
+[assembly: AssemblyFileVersion("1.7.2")]
+[assembly: MelonInfo(typeof(CanneryManufacturingMain), "CanneryManufacturing", "1.7.2", "Digitalzombie, Atlas-Lumi, ds5678, Varrxion", null)]
 
 //This tells MelonLoader that the mod is only for The Long Dark.
 [assembly: MelonGame("Hinterland", "TheLongDark")]
